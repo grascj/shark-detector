a55f8008ed1bd415700b50318a8452e69f67a9c6
grascj <christopher.grasing@stonybrook.edu>
2017-11-22 23:02:43 -0500

    screenshots and compares every 5 seconds

--- screenshotter.js
+++ screenshotter.js
@@ -1,41 +1,115 @@
 
+
 var image_dict = {};
 
-var activeTab = 0;
+var INTERVAL = 5000;
 
 
-console.log(resemble);
+function spamScreenShots(id){
+		chrome.tabs.captureVisibleTab({format : "png"}, function(img){
+			console.log("firing");
+			image_dict[id][1] = img;
+		});
+}
 
-//Check if newly opened
-//if not then we must compare to old screenshot
+//var screenshot_thread = setInterval(spamScreenShots,INTERVAL,1,"test");
+var screenshot_thread = null;
+
+
+//imagePercentDiff("People.jpg","People2.jpg", function(percentdiff) {
+//  console.log(percentdiff);
+//});
+//addDiffImage(document.body,"People.jpg","People2.jpg");
+	
+
+
+
+/*
+ *
+ * When a tab is selected one of two things can happen:
+ * 	- It's not in the dictionary and an entry needs to be made.
+ * 	- It's in the dictonary and the screenshot needs to be compared.
+ *
+ */
 chrome.tabs.onActivated.addListener(function listener(activeInfo) {
 
-	activeTab = activeInfo.tabId;
-
 	chrome.tabs.get(activeInfo.tabId, function callback(tab){
-		image_dict[tab.id] = tab.url;
-		console.log(image_dict);
+
+		var url = new URL(tab.url); 
+		
+		if(!(url.protocol == "https:" || url.protocol == "http:")){
+			return;
+		}
+
+		chrome.tabs.captureVisibleTab({format : "png"}, function(img) {
+			
+			//stop the old thread of spamming screenshots
+			clearInterval(screenshot_thread);
+
+			if(image_dict[tab.id] === undefined){
+				image_dict[tab.id] = [tab.url,img];
+				console.log(image_dict)
+			}else{
+				if(tab.active){
+					resemble(img).compareTo(image_dict[tab.id][1]).onComplete(function(data) {
+
+						console.log(data.misMatchPercentage + "% change in original page." + tab.active);
+
+						//TODO DO SOMETHING WITH THIS PERCENTAGE
+						image_dict[tab.id] = [tab.url,img];
+						console.log(image_dict)
+					});
+				}
+			}
+
+			//start up the old screenshot spam thread
+			screenshot_thread = setInterval(spamScreenShots,INTERVAL,tab.id);
+
+		});
 	});
-
-
 });
 
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+/*
+ *
+ * If a tab is deleted it's entry is removed the entry from the dictionary
+ *
+ */
+chrome.tabs.onRemoved.addListener(function (tabId, changeInfo, tab) {
 
-	//this is to avoid insane regexes
-	var url = new URL(tab.url)
-	console.log(url.protocol);
-
-	if(!tab.active || changeInfo.status!="complete"){
-		return;
-	}
-
-	image_dict[tab.id] = tab.url;
+	delete image_dict[tabId];
 	console.log(image_dict);
 });
 
-imagePercentDiff("People.jpg","People2.jpg", function(percentdiff) {
-  console.log(percentdiff);
+
+
+/*
+ *
+ * When there's a new page loaded put a new entry in the dictionary
+ *
+ */
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+
+	if(tab.active && changeInfo.status=="complete"){
+
+		var url = new URL(tab.url);
+
+		//stop the old thread of spamming screenshots
+		clearInterval(screenshot_thread);
+
+		if(url.protocol == "https:" || url.protocol == "http:"){
+			chrome.tabs.captureVisibleTab({format : "png"}, function(img) {
+				image_dict[tab.id] = [tab.url,img];
+				console.log(image_dict)
+
+				//start up the old screenshot spam thread
+				screenshot_thread = setInterval(spamScreenShots,INTERVAL,tab.id);
+			});
+		}
+	}
 });
-addDiffImage(document.body,"People.jpg","People2.jpg");
+
+
+
+
+
